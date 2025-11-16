eaffc4288919d94dd963a173769a41398d459877
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-16 15:54:34 +0800

    chore: update sponsors info

--- src/providers/manual.ts
+++ src/providers/manual.ts
@@ -1,18 +1,18 @@
 import type { Provider, Sponsorship } from 'sponsorkit'
-import { sponsorFromGitHub } from '../utils/sponsor'
+// import { sponsorFromGitHub } from '../utils/sponsor'
 
 export const manualProvider: Provider = {
     name: 'manual',
     fetchSponsors: async () => await Promise.all([
-        {
-            sponsor: sponsorFromGitHub('Interstellar750'),
-            monthlyDollars: -1, // Past Sponsor
-            isOneTime: true
-        },
-        {
-            sponsor: sponsorFromGitHub('nekomeowww'),
-            monthlyDollars: 300, // Special Sponsor
-            isOneTime: true
-        }
+        // {
+        //     sponsor: sponsorFromGitHub('Interstellar750'),
+        //     monthlyDollars: -1, // Past Sponsor
+        //     isOneTime: true
+        // },
+        // {
+        //     sponsor: sponsorFromGitHub('nekomeowww'),
+        //     monthlyDollars: 300, // Special Sponsor
+        //     isOneTime: true
+        // }
     ] satisfies Sponsorship[])
 }
